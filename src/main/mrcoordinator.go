1ec83e691ef9f72e782bea4bc0a19b1a93753b2e
1uvu <zjh98@vip.qq.com>
2021-03-09 21:21:52 +0800

    add some code comment

--- src/main/mrcoordinator.go
+++ src/main/mrcoordinator.go
@@ -20,8 +20,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	m := mr.MakeCoordinator(os.Args[1:], 10)
-	for m.Done() == false {
+	// By 1uvu
+	// 创建 Coordinator，轮询 Done()，true 说明此次运行成功
+	// 注意: Coordinator 是一个 RPC Server,一旦创建后再不主动或是触发停止的情况会一直保持执行
+	// todo 所以为了 Coordinator 可以进行多次任务, 再一次任务 Done 了后要刷新 Coordinator 的状态信息
+	//
+	c := mr.MakeCoordinator(os.Args[1:], 10)
+	for c.Done() == false {
 		time.Sleep(time.Second)
 	}
 
--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -11,26 +11,35 @@ import "os"
 import "net/rpc"
 import "net/http"
 
-//
-//  for each task, map or reduce, only
+// By 1uvu
+// mutex 信号量锁住的其实不是资源而是资源的“状态”, 一份资源上一把锁
+// 使得每一个获得了这份资源的锁的线程均可以确定地使用和修改资源的状态.
+// 也就是说，对上锁了的资源的状态的使用和修改操作是原子的.
+// 另外注意: 锁本身无意义,它保证的只是对一系列状态使用和修改操作的原子性,
+// map worker 和 reduce worker 并不会一起执行, 因此此处只需要定义一个 mutex 写好了即可.
 //
 var mutex sync.Mutex
 
 type Coordinator struct {
+	// By 1uvu
 	// 记录已分配的资源和工作进行情况
-	InputFiles []string
-	MapNumber int
-	ReduceNumber int
-	MapTaskStatusMap map[int]TaskStatus
-	ReduceTaskStatusMap map[int]TaskStatus
-	CurPhase Phase
-	TaskDone bool
-	DoneMapNumber int
-	DoneReduceNumber int
+	InputFiles 				[]string //	-------	// map worker 的输入文件
+	MapNumber 				int 	 //	-------	// map task 总数, 一个 task 对应一个 worker
+	ReduceNumber 			int 	 //	-------	// reduce task 总数, 一个 task 对应一个 worker
+	MapTaskStatusMap 		map[int]TaskStatus	// 记录每一个 map worker 的 task 当前执行状态
+	ReduceTaskStatusMap 	map[int]TaskStatus	// 记录每一个 reduce worker 的 task 当前执行状态
+	CurPhase 				Phase 	 //	-------	// 当前处于的任务阶段: map/reduce
+	TaskDone 				bool 	 //	-------	// 任务是否已完成
+	DoneMapNumber 			int 	 //	-------	// 已完成的 map task 总数
+	DoneReduceNumber 		int 	 //	-------	// 已完成的 reduce task 总数
 }
 
 // Your code here -- RPC handlers for the worker to call.
+// By 1uvu
+// 根据 worker 的请求参数携带的 worker 当前状态, 来做对应的处理
+//
 func (c *Coordinator) Response(args *CallArgs, reply *CallReply) error {
+
 	switch args.CurStatus {
 	case Idle:
 		if c.TaskDone {
@@ -53,10 +62,18 @@ func (c *Coordinator) Response(args *CallArgs, reply *CallReply) error {
 	return nil
 }
 
+// By 1uvu
+// 进行 map worker 的 task 分配工作
+//
 func (c *Coordinator) HandleMapRequire(reply *CallReply) error {
-	// based the worker phase to handout task i.e. input files
 	mutex.Lock()
-	defer mutex.Unlock()
+	defer mutex.Unlock() // 此函数退出后解锁信号量
+	// task 分配步骤
+	// 1 从 MapTaskStatusMap 顺序查找第一个 NotStart 的 task
+	// 2 填充 reply
+	// 3 更新 MapTaskStatusMap 当前 task 状态为 Doing
+	// 4 启动一个定时器 goroutine 来实现简单的容错, 当前 task 的状态如果处于 Doing 超过 10s
+	// 	则认为执行它的 map worker crash 掉了, 将其状态修改为 NotStart
 	for idx:=0; idx<len(c.MapTaskStatusMap); idx++ {
 		if c.MapTaskStatusMap[idx] == NotStart {
 			reply.MapFile = c.InputFiles[idx]
@@ -82,6 +99,8 @@ func (c *Coordinator) HandleMapRequire(reply *CallReply) error {
 	return nil
 }
 
+// By 1uvu
+// 与 map task 分配过程类似
 func (c *Coordinator) HandleReduceRequire(reply *CallReply) error {
 	// based the worker phase to handout task i.e. input files
 	mutex.Lock()
@@ -110,7 +129,8 @@ func (c *Coordinator) HandleReduceRequire(reply *CallReply) error {
 	return nil
 }
 
-
+// By 1uvu
+// task Finished 时更新 Done*Number, *TaskStatusMap 和 TaskDone
 func (c *Coordinator) HandleFinished(args *CallArgs) error {
 	mutex.Lock()
 	defer mutex.Unlock()
@@ -131,6 +151,8 @@ func (c *Coordinator) HandleFinished(args *CallArgs) error {
 	return nil
 }
 
+// By 1uvu
+// task Failed 时更新 *TaskStatusMap
 func (c *Coordinator) HandleFailed(args *CallArgs) error {
 	mutex.Lock()
 	defer mutex.Unlock()
