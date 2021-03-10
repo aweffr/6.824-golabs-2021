4753d6424377b3ae14e96a9ffc505f204096c41a
1uvu <zjh98@vip.qq.com>
2021-03-10 20:08:42 +0800

    update lecture 2 note and code comment

--- docs/lectures/l02/crawler.go
+++ docs/lectures/l02/crawler.go
@@ -12,17 +12,20 @@ import (
 
 //
 // Serial crawler
+// By 1uvu
+// url 是当前待获取的 url
+// fetched 保存 url 的获取状态
 //
-
 func Serial(url string, fetcher Fetcher, fetched map[string]bool) {
 	if fetched[url] {
 		return
 	}
-	fetched[url] = true
+	fetched[url] = true // 更新当前 url 为已被获取
 	urls, err := fetcher.Fetch(url)
 	if err != nil {
 		return
 	}
+	// 用递归实现 DFS 获取 page 包含的每个 url
 	for _, u := range urls {
 		Serial(u, fetcher, fetched)
 	}
@@ -31,14 +34,17 @@ func Serial(url string, fetcher Fetcher, fetched map[string]bool) {
 
 //
 // Concurrent crawler with shared state and Mutex
-//
-
+// By 1uvu
+// 定义获取状态
+// mu 原语为修改状态上锁
+// fetched 记录每个 url string 是否已被获取
 type fetchState struct {
 	mu      sync.Mutex
 	fetched map[string]bool
 }
 
 func ConcurrentMutex(url string, fetcher Fetcher, f *fetchState) {
+	// 使用或修改状态时上锁
 	f.mu.Lock()
 	already := f.fetched[url]
 	f.fetched[url] = true
@@ -52,9 +58,11 @@ func ConcurrentMutex(url string, fetcher Fetcher, f *fetchState) {
 	if err != nil {
 		return
 	}
+	// 并发获取当前 page 的 urls
+	// done 是一个 WaitGroup, 用来存放处于阻塞状态的 goroutine 线程
 	var done sync.WaitGroup
 	for _, u := range urls {
-		done.Add(1)
+		done.Add(1) // 一个 Add 需要一个 Done 来平衡
 		//u2 := u
 		//go func() {
 		// defer done.Done()
@@ -63,7 +71,7 @@ func ConcurrentMutex(url string, fetcher Fetcher, f *fetchState) {
 		go func(u string) {
 			defer done.Done()
 			ConcurrentMutex(u, fetcher, f)
-		}(u)
+		}(u) // 带有参数的匿名函数, 启动 goroutine 来执行
 	}
 	done.Wait()
 	return
@@ -81,21 +89,26 @@ func makeState() *fetchState {
 
 func worker(url string, ch chan []string, fetcher Fetcher) {
 	urls, err := fetcher.Fetch(url)
+	// "<-" 符号前面代表消息的接收者, 后面代表发生者
 	if err != nil {
-		ch <- []string{}
+		ch <- []string{} // channel 中已无 urls 待获取
 	} else {
-		ch <- urls
+		ch <- urls // 将 urls 放入通道
 	}
 }
 
 func coordinator(ch chan []string, fetcher Fetcher) {
-	n := 1
+	n := 1 // n 记录通道中的 urls 数, ch 初始包含一个 url
 	fetched := make(map[string]bool)
-	for urls := range ch {
+	for urls := range ch { // 遍历 ch, 唤起 worker goroutine 线程
 		for _, u := range urls {
 			if fetched[u] == false {
 				fetched[u] = true
 				n += 1
+				// 启动线程来执行 ch, 如果不使用线程启动,
+				// 会由于 for 循环的多个 worker 同时等待 ch 引发死锁
+				// 尝试
+				// worker(u, ch, fetcher)
 				go worker(u, ch, fetcher)
 			}
 		}
@@ -109,8 +122,11 @@ func coordinator(ch chan []string, fetcher Fetcher) {
 func ConcurrentChannel(url string, fetcher Fetcher) {
 	ch := make(chan []string)
 	go func() {
-		ch <- []string{url}
+		ch <- []string{url} // 启动一个 channel 线程
 	}()
+	// coordinator 无法通过线程来启动, 因为不能在线程中启动其它线程
+	// 只能在当前进程中直接运行, 尝试:
+	// go coordinator(ch, fetcher)
 	coordinator(ch, fetcher)
 }
 
@@ -139,6 +155,8 @@ type Fetcher interface {
 }
 
 // fakeFetcher is Fetcher that returns canned results.
+// By 1uvu
+// 保存每个 page 获取到的 content 和 urls
 type fakeFetcher map[string]*fakeResult
 
 type fakeResult struct {
@@ -146,6 +164,8 @@ type fakeResult struct {
 	urls []string
 }
 
+// By 1uvu
+// 为 fakeFetcher 类型实现 Fetcher 接口
 func (f fakeFetcher) Fetch(url string) ([]string, error) {
 	if res, ok := f[url]; ok {
 		fmt.Printf("found:   %s\n", url)
@@ -156,6 +176,8 @@ func (f fakeFetcher) Fetch(url string) ([]string, error) {
 }
 
 // fetcher is a populated fakeFetcher.
+// By 1uvu
+// 初始化一个 fetcher
 var fetcher = fakeFetcher{
 	"http://golang.org/": &fakeResult{
 		"The Go Programming Language",
